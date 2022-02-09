881d5c7f15a7b94520dfcad31d936677bd2d35a2
beyondbounty <98137236+beyondbounty@users.noreply.github.com>
2022-02-08 17:36:35 -0800

    Add files via upload
    # Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.

---
+++ ex_02_03.py
@@ -0,0 +1,6 @@
+# Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+
+hr = float(input('Enter hours: '))
+rt = float(input('Enter rate: '))
+
+print('Pay:', (hr * rt))
